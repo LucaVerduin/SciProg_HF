81f0729c5b8d4707f26212973242d18255768e12
Luca Verduin <ld.verduin@gmail.com>
2026-03-16 00:26:05 +0100

    Moved most of Hartree Fock operations (core hamiltonian/SCF) into HartreeFock.f90

--- src/main.F90
+++ src/main.F90
@@ -18,14 +18,8 @@ program HartreeFock
 
      ! Variable naming as in the description of the exercise
      integer  :: n_AO, n_occ
-     integer  :: kappa, lambda, mu, nu
-     integer :: max_cycles, icycle
-     real(8)  :: E_HF
-     real(8), allocatable :: H(:,:), F(:,:), V(:,:),T(:,:),S(:,:), C(:,:), eps(:), D(:,:), D_old(:,:)
-     real(8) :: delta_D, tolerance
-
-     ! The following large array can be eliminated when Fock matrix contruction is implemented
-     real(8), allocatable :: ao_integrals (:,:,:,:)
+     integer :: max_cycles
+     real(8) :: tolerance
    
      ! Definition of the molecule
      call define_molecule(molecule)
@@ -37,77 +31,12 @@ program HartreeFock
      ! Definition of the number of occupied orbitals
      n_occ = 3 ! hardwired for this demonstration program, should be set via input
 
-     ! Compute the overlap matrix
-     allocate (S(n_AO,n_AO))
-     call   compute_1e_integrals ("OVL",ao_basis,ao_basis,S)
-
-     ! Compute the kinetic matrix
-     allocate (T(n_AO,n_AO))
-     call   compute_1e_integrals ("KIN",ao_basis,ao_basis,T)
-
-     ! Compute the potential matrix
-     allocate (V(n_AO,n_AO))
-     call   compute_1e_integrals ("POT",ao_basis,ao_basis,V,molecule)
-
-     ! Compute the core Hamiltonian matrix (the potential is positive, we scale with -e = -1 to get to the potential energy matrix)
-     allocate (F(n_AO,n_AO))
-     allocate (H(n_AO,n_AO))
-     H = T - V
-
-     ! Diagonalize the Fock matrix
-     allocate (C(n_AO,n_AO))
-     allocate (eps(n_AO))
-     call solve_genev (H,S,C,eps)
-     print*, "Orbital energies for the core Hamiltonian:",eps
-
-     ! Form the density matrix
-     allocate (D(n_AO,n_AO))
-     allocate (D_old(n_AO,n_AO))
-
-     call calculateDensity(C,D,n_occ)
-
-     allocate (ao_integrals(n_AO,n_AO,n_AO,n_AO))
-     ! Compute all 2-electron integrals
-     call generate_2int (ao_basis,ao_integrals)
-
-
+    call coreHamiltonian(n_AO, n_occ, molecule, ao_basis)
 
     tolerance = 0.0001d0
-    icycle = 0
     max_cycles = 1000
 
-    ! SCF loop
-    do
-      
-      ! Calculating Fock matrix from D
-      do lambda = 1, n_ao
-        do kappa = 1, n_ao
-              F(kappa, lambda) = H(kappa, lambda) + sum( (2.d0*ao_integrals(:,:,kappa,lambda) - ao_integrals(:,lambda,kappa,:))*D )
-        end do
-      end do
-
-      ! Saving copy of previous density matrix and calculating new density matrix
-      D_old = D
-      call solve_genev(F,S,C,eps)
-      call calculateDensity(C,D,n_occ)
-
-      ! Calculate energy of this itteration
-      E_HF = sum(H*D)         ! Hcore part
-      E_HF = E_HF + sum(F*D)  ! F part
-
-      print '(a,2x,i4,2x,f14.8,a)', "Energy cycle ", icycle, E_HF, " Ha" 
-
-      icycle = icycle + 1 ! Count # cycles
-
-      ! Calculating delta_D and checking for convergence
-      delta_D = sqrt(sum( (D_old - D)**2 ))
-      if (delta_D < tolerance .or. icycle==max_cycles) then
-        print '(a,t13,i4,t18,a)', "Converged in", icycle, "cycles"
-        exit
-      end if 
-
-    end do
-    ! End SCF loop
+    call SCFprocedure(n_AO, n_occ, max_cycles, tolerance)
    
      print*, "The Hartree-Fock energy:    ", E_HF
 
